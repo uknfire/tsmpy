3573c13853d41f80bb94346f94544b8fee6059d4
weng <rawfh0@gmail.com>
2021-04-11 00:12:13 +0800

    Add new methods to TSM.py

--- topology_shape_metrics/TSM.py
+++ topology_shape_metrics/TSM.py
@@ -3,21 +3,45 @@
 from topology_shape_metrics.planarization import Planarization
 from topology_shape_metrics.orthogonalization import Orthogonalization
 from topology_shape_metrics.compaction import Compaction
+from topology_shape_metrics.utils import number_of_cross
 import networkx as nx
 
+
 class TSM:
-    def __init__(self, G, pos=None, precheck=True):
-        if precheck:
-            Planarization.precheck(G, pos)
+    def __init__(self, G, pos=None, checkit=True, usepulp=False):
+        if checkit:
+            TSM.precheck(G, pos)
+
         self.planar = Planarization(G, pos)
         self.ortho = Orthogonalization(self.planar)
+        if usepulp:
+            self.ortho.lp_solve()
         self.compa = Compaction(self.ortho)
+
         self.G = G
         self.pos = self.compa.pos
 
-    def check(self):
+    def postcheck(self):
         for u, v in self.planar.G.edges:
             assert self.pos[u][0] == self.pos[v][0] or self.pos[u][1] == self.pos[v][1]
 
     def draw(self, **kwds):
         nx.draw(self.planar.G, self.pos, **kwds)
+
+    @staticmethod
+    def precheck(G, pos=None):
+        if max(degree for node, degree in G.degree) > 4:
+            raise Exception(
+                "Max node degree larger than 4, which is not supported currently")
+        if nx.number_of_selfloops(G) > 0:
+            raise Exception("G contains selfloop")
+        if not nx.is_connected(G):
+            raise Exception("G is not a connected graph")
+
+        if pos is None:
+            is_planar, _ = nx.check_planarity(G)
+            if not is_planar:
+                raise Exception("G is not a planar graph")
+        else:
+            if number_of_cross(G, pos) > 0:
+                raise Exception("There are cross edges in pos")
