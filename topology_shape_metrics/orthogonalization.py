851e267d7d5fd10f3ee9d2fa43ef199672d566a2
weng <rawfh0@gmail.com>
2021-04-11 00:13:31 +0800

    Rewrite lp_solve

--- topology_shape_metrics/orthogonalization.py
+++ topology_shape_metrics/orthogonalization.py
@@ -3,14 +3,10 @@ from collections import defaultdict
 from topology_shape_metrics.flownet import Flow_net
 
 class Orthogonalization:
-    '''
-    works on a planar embedding, changes shape of the graph.
+    '''works on a planar embedding, changes shape of the graph.
     '''
 
     def __init__(self, planar):
-        assert max(pair[1] for pair in planar.G.degree) <= 4
-        assert planar.G.number_of_nodes() > 1
-
         self.planar = planar
 
         self.flow_network = self.face_determination()
@@ -38,9 +34,7 @@ class Orthogonalization:
     def tamassia_orthogonalization(self):
         return self.flow_network.min_cost_flow()
 
-    def lp_solve(self, weight_of_corner=1,
-                 # trans=lambda s: s if s[0] != '(' else eval(s.replace('_', ' ')),
-                 ):
+    def lp_solve(self, weight_of_corner=1):
         '''
         Use linear programming to solve min cost flow problem, make it possible to define constrains.
 
@@ -51,22 +45,24 @@ class Orthogonalization:
 
         prob = pulp.LpProblem()  # minimize
 
-        var_dict = defaultdict(lambda: defaultdict(dict))
-
+        var_dict = {}
+        varname2tuple = {}
         for u, v, he_id in self.flow_network.edges:
-            var_dict[u][v][he_id] = pulp.LpVariable(
-                f'{u}%{v}%{he_id}',
+            var_dict[u, v, he_id] = pulp.LpVariable(
+                f'{u}{v}{he_id}',
                 self.flow_network[u][v][he_id]['lowerbound'],
                 self.flow_network[u][v][he_id]['capacity'],
                 pulp.LpInteger
             )
+            # pulp will replace ' ' with '_' automatically
+            varname2tuple[f'{u}{v}{he_id}'.replace(' ', "_")] = (u, v, he_id)
 
         objs = []
         for he in self.planar.dcel.half_edges.values():
             lf, rf = he.twin.inc.id, he.inc.id
             objs.append(
                 self.flow_network[lf][rf][he.id]['weight'] *
-                var_dict[lf][rf][he.id]
+                var_dict[lf, rf, he.id]
             )
 
         # bend points' cost
@@ -76,40 +72,34 @@ class Orthogonalization:
                     (f1, he1_id), (f2, he2_id) = [(f, key)
                                                   for f, keys in self.flow_network.adj[v].items()
                                                   for key in keys]
-                    x = var_dict[v][f1][he1_id]
-                    y = var_dict[v][f2][he2_id]
+                    x = var_dict[v, f1, he1_id]
+                    y = var_dict[v, f2, he2_id]
                     p = pulp.LpVariable(
-                        x.name + "%temp", None, None, pulp.LpInteger)
+                        x.name + "temp", None, None, pulp.LpInteger)
                     prob.addConstraint(x - y <= p)
                     prob.addConstraint(y - x <= p)
                     objs.append(weight_of_corner * p)
 
-        prob += pulp.lpSum(objs), "number of bends in graph"
+        prob += pulp.lpSum(objs) # number of bends in graph
 
         for f in self.planar.dcel.faces:
             prob += self.flow_network.nodes[f]['demand'] == pulp.lpSum(
-                [var_dict[v][f][he_id] for v, _, he_id in self.flow_network.in_edges(f, keys=True)])
+                [var_dict[v, f, he_id] for v, _, he_id in self.flow_network.in_edges(f, keys=True)])
         for v in self.planar.G:
             prob += -self.flow_network.nodes[v]['demand'] == pulp.lpSum(
-                [var_dict[v][f][he_id] for _, f, he_id in
+                [var_dict[v, f, he_id] for _, f, he_id in
                  self.flow_network.out_edges(v, keys=True)]
             )
 
         state = prob.solve()
         if state == 1:  # update flow_dict
-            # code here works only when nodes are represented by str, likes '(1, 2)'
             for var in prob.variables():
-                if 'temp' not in var.name:
-                    l = var.name.split('%')
-                    if len(l) == 3:
-                        # u, v, he_id = map(trans, l) # change str to tuple !!!!!!!!!
-                        u, v, he_id = [item.replace('_', ' ') for item in l]
-                        he_id = eval(he_id)
-                        self.flow_dict[u][v][he_id] = int(var.varValue)
-            return pulp.value(prob.objective)
-        else:
-            return 2**32
+                if var.name in varname2tuple:
+                    u, v, he_id = varname2tuple[var.name]
+                    self.flow_dict[u][v][he_id] = int(var.varValue)
 
+
+    # Only for validation
     def number_of_corners(self):
         count_right_angle = 0
         for node in self.planar.G:
